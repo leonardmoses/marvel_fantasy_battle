f6547b312774b6305e9eb0d6d7669853b60c317e
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-09-14 00:16:58 -0400

    Added powergrid sample data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { powergrid } from './data/powergrid';
+
 function App() {
+  console.log(powergrid)
   return (
     <div className="App">
       <h1>Marvel Fantasy Battle</h1>
+      <div>{powergrid.map((x) => (
+        <ul key={x.id}>
+          <li>Name: {x.name}</li>
+          <li>id: {x.id}</li>
+          <li>Draft Value: {x.draft_value}</li>
+          <li>Mele: {x.mele_effectiveness}</li>
+        </ul>
+        
+      ))}</div>
     </div>
   );
 }
