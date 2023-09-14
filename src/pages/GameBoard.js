370c48459b040ff1beb38efdd7c8ff33cbdc7737
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-09-14 14:16:50 -0400

    Before to-do-list

--- src/pages/GameBoard.js
+++ src/pages/GameBoard.js
@@ -1,13 +1,21 @@
-import { powergrid } from '../data/powergrid';
+import { powergrid } from "../data/powergrid";
 
 const GameBoard = () => {
   return (
-    <div>
-        <h1 className='text-3xl font-bold underline'>Game Board</h1>
-        <div>
-            <label>Choose Character</label>
-            <input type='text'/>
-        </div>
+    <div className="m-5">
+      <h2 className="text-5xl font-bold">Game Board</h2>
+      <form className="mt-5">
+        <label><h4 className="text-2xl">Choose Character</h4></label>
+        <input
+          type="text"
+          placeholder="Name"
+          className="mt-1 block w-1/3 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        />
+      </form>
     </div>
   );
 };
