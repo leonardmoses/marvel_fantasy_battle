069dcfe3384342e256788362071e743da2f23439
Qalexeon <leonardsamuelmoses@gmail.com>
2023-09-19 15:38:44 -0400

    Fixed undefined error. Error occured when selecting stage before selecting team. This threw an error because forEach loop was trying to access teamA before teamA was created so it returned undefined. Fixed by adding option chain operator '?' after teamA on line 16. Error no longer occurs.

--- src/components/stages/StageClassRoom.js
+++ src/components/stages/StageClassRoom.js
@@ -1,32 +1,31 @@
 
 const StageClassRoom = ({teamA}) => {
-    // console.log(teamA)
+    console.log(teamA)
 
-    // // How the stage modifies the individual stats. All values should add up to 1.0
-    // const stageModifier = {
-    //     mele_effectiveness: 0.25,
-    //     projectile_effectiveness: 0.25,
-    //     durability: 0.25,
-    //     flight_speed: 0.25
-    // }
+    // How the stage modifies the individual stats. All values should add up to 1.0
+    const stageModifier = {
+        mele_effectiveness: 0.25,
+        projectile_effectiveness: 0.25,
+        durability: 0.25,
+        flight_speed: 0.25
+    }
 
-    // // Create a copy of all the team stats for the stage
-    // const teamAStage = []
-    // // Creates an actual copy instead of reference in memory
-    // teamA.forEach(teamAMember => {
-    //     let copy = Object.assign({} , teamAMember)
-    //     teamAStage.push(copy)
-    // });
+    // Create a copy of all the team stats for the stage
+    const teamAStage = []
+    // Creates an actual copy instead of reference in memory
+    teamA?.forEach(teamAMember => {
+        let copy = Object.assign({} , teamAMember)
+        teamAStage.push(copy)
+    });
 
-    // // Executing the Stage modifications
-    // teamAStage.forEach(teamAMember => {
-    //     teamAMember.mele_effectiveness = teamAMember.mele_effectiveness * stageModifier.mele_effectiveness
-    //     teamAMember.projectile_effectiveness = teamAMember.projectile_effectiveness * stageModifier.projectile_effectiveness
-    //     teamAMember.durability = teamAMember.durability * stageModifier.durability
-    //     teamAMember.flight_speed = teamAMember.flight_speed * stageModifier.flight_speed
+    // Executing the Stage modifications
+    teamAStage.forEach(teamAMember => {
+        teamAMember.mele_effectiveness = teamAMember.mele_effectiveness * stageModifier.mele_effectiveness
+        teamAMember.projectile_effectiveness = teamAMember.projectile_effectiveness * stageModifier.projectile_effectiveness
+        teamAMember.durability = teamAMember.durability * stageModifier.durability
+        teamAMember.flight_speed = teamAMember.flight_speed * stageModifier.flight_speed
 
-    // });
-
+    });
 
     // console.log(teamAStage)
 
